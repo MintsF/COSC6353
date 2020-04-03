86d71ae8c27c2a8c7054f09cf83f20d2c6412224
MintsF <42982813+MintsF@users.noreply.github.com>
2020-04-02 20:11:18 -0500

    Update tests.py

--- user/tests.py
+++ user/tests.py
@@ -5,7 +5,7 @@ from user.models import UserInfo
 from django.test import Client
 from user import views
 import json
-
+from user.models import Profile
 
 class UserInfoModelTest(TestCase):
 	def setUp(self):
@@ -59,7 +59,57 @@ class RegisterModelTest(TestCase):
 		response=c.post('/api/register/',{"username":"11223344","password": "124"})
 		self.assertEqual(json.loads(response.content.decode())['code'],2002)
 
-class FuelQuoteModelTest(TestCase):
+class ProfileModelTest(TestCase):
+	def setUp(self):
+		Profile.objects.create(username='11223344',fullname='Mike',address1='North Stadium', address2='',city='Houston', state='TX',zipcode='77700')
+		UserInfo.objects.create(username= '11223344', password="123")
+
+	def testProfileInsert(self):
+		c= Client()
+		response=c.post('/api/profile/',{"userid":"11223355","username":"Mike","address1":"North Stadium", "address2":"3333","city":"Houston", "state":"TX","zipcode":"77700"})
+		self.assertEqual(json.loads(response.content.decode())['code'],3001)
+		self.assertEqual(json.loads(response.content.decode())['username'],"Mike")
+		self.assertEqual(json.loads(response.content.decode())['address1'],"North Stadium")
+		self.assertEqual(json.loads(response.content.decode())['address2'],"3333")
+		self.assertEqual(json.loads(response.content.decode())['city'],"Houston")
+		self.assertEqual(json.loads(response.content.decode())['state'],"TX")
+		self.assertEqual(json.loads(response.content.decode())['zipcode'],"77700")
+
+	def testProfileUpdate(self):
+		c=Client()
+		response = c.post('/api/profile/',{"userid":"\"11223344\"","username":"Joe","address1":"North Stadium1", "address2":"4444","city":"Austin", "state":"CA","zipcode":"777009999"})
+		self.assertEqual(json.loads(response.content.decode())['address2'],"4444")
+		self.assertEqual(json.loads(response.content.decode())['code'],3002)
+		self.assertEqual(json.loads(response.content.decode())['address1'],"North Stadium1")
+		self.assertEqual(json.loads(response.content.decode())['city'],"Austin")
+		self.assertEqual(json.loads(response.content.decode())['state'],"CA")
+		self.assertEqual(json.loads(response.content.decode())['zipcode'],"777009999")
+		self.assertEqual(json.loads(response.content.decode())['username'],"Joe")
+		self.assertEqual(json.loads(response.content.decode())['userid'],"11223344")
+
+	def testInitProfie(self):
+		c=Client()
+		UserInfo.objects.create(username= '11223366', password="123")
+		response=c.post('/api/login/',{"username":"11223366","password":"123"})
+		self.assertEqual(json.loads(response.content.decode())['flag'],0)
+		response=c.post('/api/initProfile/',{"userid":"\"11223366\""})
+		self.assertEqual(json.loads(response.content.decode())['code'],5003)
+
+	def testChangPassword(self):
+		c=Client()
+		response=c.post('/api/changepassword/',{"username":"\"11223344\"","password":"123","newPassword":"134"})
+		self.assertEqual(json.loads(response.content.decode())['code'],4002)
+		self.assertEqual(json.loads(response.content.decode())['password'],"134")
+
+	def testChangePasswordOldPasswordError(self):
+		c=Client()
+		response= c.post('/api/changepassword/',{"username":"\"11223344\"","password":"124","newPassword":"134"})
+		self.assertEqual(json.loads(response.content.decode())['code'],4001)
+
+
+
+
+# class FuelQuoteModelTest(TestCase):
 	
 		
 
